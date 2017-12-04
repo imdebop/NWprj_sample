30107cc483ed97fb44da21ce695b4fcbd7c5f119
imdebop <daiho_hayashi@nifty.ocm>
2017-12-04 13:04:39 +0900

    nayose

--- scripts/init.js
+++ scripts/init.js
@@ -1,9 +1,17 @@
 
 $(function() {
     $('#opPanel').load('./panel_sho',function(){
-        $('#kana_kensaku').on('click','.kana', function ($this){
-            s_text = $this.target.textContent
-            $('#content').load("./sho_kana?kana=" + s_text);
+        $('#kana_kensaku').on('click','.kana',
+            function ($this){ s_text = $this.target.textContent
+            $('#content').load("./sho_kana?kana=" + s_text,
+                function() {
+                    $('#sho_list').on('click','.shoyusha', function ($this){
+                        s_text = $this.target.textContent
+                        kana_sub(s_text);
+                        //$('#content').load("./sho_kana?kana=" + s_text);
+                    });
+            
+            });
         });
     });
 
