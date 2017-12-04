4801f4b52e6ffd59c594ff0a55be792253c74136
imdebop <daiho_hayashi@nifty.ocm>
2017-12-04 11:13:13 +0900

    scroll

--- scripts/init.js
+++ scripts/init.js
@@ -1,12 +1,24 @@
 
 $(function() {
     $('#opPanel').load('./panel_sho',function(){
-        $('#kana_kensaku').on('click',alert())
+        $('#kana_kensaku').on('click','.kana', function ($this){
+            s_text = $this.target.textContent
+            $('#content').load("./sho_kana?kana=" + s_text);
+        });
+    });
+
+    var kana_sub = function(obj){
+        console.log(obj);
     }
-    );
+
 });
 
 $(function() {
     $('#content').load('./content_top');
 });
+
+
+//$(function(){
+//    $('#opPanel').on('click','.kana',alert())
+//});
 
